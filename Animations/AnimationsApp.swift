e95cba3cf32c0b71680075546559841700b154f4
Daniel Gutierrez <danielgudu22@gmail.com>
2021-09-06 12:09:15 -0500

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Daniel Gutierrez on 6/09/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
